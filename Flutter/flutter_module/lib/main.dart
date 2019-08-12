3e8bc1edb9a59eb2a60b7847f73a1a7a529f965c
HZW <>
2019-08-12 16:27:05 +0800

    [Utility]  添加Flutter界面

--- Flutter/flutter_module/lib/main.dart
+++ Flutter/flutter_module/lib/main.dart
@@ -1,35 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boost/flutter_boost.dart';
+import 'TestFlutterJumpFlutter.dart';
+import 'simple_page_widgets.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_module/TestListView.dart';
 
-void main(List<String> args) {
+typedef void NativeCallBack(dynamic object);
+final Map<String, NativeCallBack> native_flutter_callbakcs =
+    <String, NativeCallBack>{};
+
+const EventChannel eventChannel =
+    const EventChannel("samples.flutter.io/nativeCallFlutter");
+const MethodChannel platform =
+    const MethodChannel("samples.flutter.io/flutterCallNative");
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
+  @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  void _onEvent(Object event) {
+    if (event is Map) {
+      String name = event['name'];
+      if (native_flutter_callbakcs[name] != null) {
+        native_flutter_callbakcs[name](event);
+      }
+    }
+  }
+
+  void _onError(Object error) {
+    setState(() {
+      // _changingStatus = "${error}";
+    });
+  }
+
   @override
   void initState() {
+    // 接收Native调用
+    eventChannel.receiveBroadcastStream().listen(_onEvent, onError: _onError);
     super.initState();
 
     FlutterBoost.singleton.registerPageBuilders({
-      // 'sample://firstPage': (pageName, params, _) => FirstRouteWidget(),
-      // 'sample://secondPage': (pageName, params, _) => SecondRouteWidget(),
-    });
+      'first': (pageName, params, _) => FirstRouteWidget(),
+      'second': (pageName, params, _) => SecondRouteWidget(),
+      'tab': (pageName, params, _) => TabRouteWidget(),
+      'flutterFragment': (pageName, params, _) => FragmentRouteWidget(params),
 
+      ///可以在native层通过 getContainerParams 来传递参数
+      'flutterPage': (pageName, params, _) {
+        print("flutterPage params:$params");
+
+        return FlutterRouteWidget();
+      },
+    });
+    FlutterBoost.singleton.registerPageBuilders({
+      'testList': (pageName, params, _) => TestListView(),
+      'TestFlutterJumpFlutter': (pageName, params, _) =>
+          TestFlutterJumpFlutter()
+    });
     FlutterBoost.handleOnStartPage();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Boost example",
-      builder: FlutterBoost.init(),
-      home: Container(),
-    );
+        title: 'Flutter Boost example',
+        builder: FlutterBoost.init(postPush: _onRoutePushed),
+        home: Container());
+  }
+
+  void _onRoutePushed(
+      String pageName, String uniqueId, Map params, Route route, Future _) {
+//    List<OverlayEntry> newEntries = route.overlayEntries
+//        .map((OverlayEntry entry) => OverlayEntry(
+//            builder: (BuildContext context) {
+//              final pageWidget = entry.builder(context);
+//              return Stack(
+//                children: <Widget>[
+//                  pageWidget,
+//                  Positioned(
+//                    child: Text(
+//                      "pageName:$pageName\npageWidget:${pageWidget.toStringShort()}",
+//                      style: TextStyle(fontSize: 12.0, color: Colors.red),
+//                    ),
+//                    left: 8.0,
+//                    top: 8.0,
+//                  )
+//                ],
+//              );
+//            },
+//            opaque: entry.opaque,
+//            maintainState: entry.maintainState))
+//        .toList(growable: true);
+//
+//    route.overlayEntries.clear();
+//    route.overlayEntries.addAll(newEntries);
   }
 }
