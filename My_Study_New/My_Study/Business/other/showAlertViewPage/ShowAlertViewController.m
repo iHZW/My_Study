5f4cd82c3953a585eb93fa615c0831d3e867c028
HZW <18516638588@163.com>
2022-11-17 13:34:03 +0800

    122221

--- My_Study_New/My_Study/Business/other/showAlertViewPage/ShowAlertViewController.m
+++ My_Study_New/My_Study/Business/other/showAlertViewPage/ShowAlertViewController.m
@@ -29,16 +29,16 @@
 @implementation ShowAlertViewController
 
 
--  (void)initExtendedData
+ -   (void)initExtendedData
     {
-     [super initExtendedData];
+      [super initExtendedData];
     
- // self.dataArray = [NSMutableArray arrayWithArray:[self getDataArray]];
-   self.tableCellClass = [PASIndicatorTableViewCell class];
-         self.style = UITableViewStylePlain;
- self.cellHeight = 60  ;
-         self.title = @"展示Alert";
-     }
+  // self.dataArray = [NSMutableArray arrayWithArray:[self getDataArray]];
+    self.tableCellClass = [PASIndicatorTableViewCell class];
+          self.style = UITableViewStylePlain;
+  self.cellHeight = 60  ;
+          self.title = @"展示Alert";
+      }
 
 
 - (void)loadUIData
