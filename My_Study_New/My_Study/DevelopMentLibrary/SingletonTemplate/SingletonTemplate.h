281d42de7d6a3a44971f1363449d93ea51d28974
hzw <zhiwei.han01@weimob.com>
2023-12-19 08:44:04 +0800

    Revert "Modify: 修改单利宏写法"
    This reverts commit 925c51a1447cf0b25c0c5d5493f9da7dfb13ba97.

--- My_Study_New/My_Study/DevelopMentLibrary/SingletonTemplate/SingletonTemplate.h
+++ My_Study_New/My_Study/DevelopMentLibrary/SingletonTemplate/SingletonTemplate.h
@@ -12,12 +12,12 @@
 
 #define DEFINE_SINGLETON_T_FOR_HEADER(className) \
 \
-+ (className *)sharedInstance;
++ (className *)shared##className;
 
 #if !__has_feature(objc_arc)
 #define DEFINE_SINGLETON_T_FOR_CLASS(className) \
 \
-+ (className *)sharedInstance { \
++ (className *)shared##className { \
 static className *shared##className = nil; \
 static dispatch_once_t onceToken; \
 dispatch_once(&onceToken, ^{ \
@@ -51,27 +51,14 @@ return self;\
 #else
 #define DEFINE_SINGLETON_T_FOR_CLASS(className) \
 \
-+ (className *)sharedInstance { \
++ (className *)shared##className { \
 static className *shared##className = nil; \
 static dispatch_once_t onceToken; \
 dispatch_once(&onceToken, ^{ \
-shared##className = [[super allocWithZone:NULL] init]; \
+shared##className = [[[self class] alloc] init]; \
 }); \
 return shared##className; \
-}\
-\
-+ (instancetype)allocWithZone:(struct _NSZone *)zone {\
-return [self sharedInstance];\
-}\
-\
-- (instancetype)copy {\
-return self;\
-}\
-\
-- (instancetype)mutableCopy {\
-return self;\
-}\
-
+}
 #endif
 
 #endif /* SINGLETONTEMPLATE_H */
